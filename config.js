313a6b3df3fa4d5d03979280cea8cbb911fcb15a
Siddise Hirpa <siddisehirpa@gmail.com>
2019-12-12 16:49:07 +0100

    Change configs to fit with oslo pride

--- config.js
+++ config.js
@@ -1,45 +1,40 @@
 const config = {
-	"gatsby": {
-		"pathPrefix": "/",
-		"siteUrl": "https://learn.hasura.io",
-		"gaTrackingId": null
-	},
-	"header": {
-		"logo": "https://graphql-engine-cdn.hasura.io/learn-hasura/assets/homepage/favicon.png",
-		"logoLink": "https://learn.hasura.io",
-		"title": "Gatsby Gitbook Boilerplate",
-		"githubUrl": "https://github.com/hasura/gatsby-gitbook-boilerplate",
-		"helpUrl": "",
-		"tweetText": "",
-		"links": [
-			{ "text": "", "link": ""}
-		],
-		"search": {
-			"enabled": false,
-			"indexName": "",
-			"algoliaAppId": process.env.GATSBY_ALGOLIA_APP_ID,
-			"algoliaSearchKey": process.env.GATSBY_ALGOLIA_SEARCH_KEY,
-			"algoliaAdminKey": process.env.ALGOLIA_ADMIN_KEY
-		}
-	},
-	"sidebar": {
-		"forcedNavOrder": [
-			"/introduction",
-    		"/codeblock"
-		],
-		"links": [
-			{ "text": "Hasura", "link": "https://hasura.io"},
-		],
-		"frontline": false,
-		"ignoreIndex": true,
-	},
-	"siteMetadata": {
-		"title": "Gatsby Gitbook Boilerplate | Hasura",
-		"description": "Documentation built with mdx. Powering learn.hasura.io ",
-		"ogImage": null,
-		"docsLocation": "https://github.com/hasura/gatsby-gitbook-boilerplate/tree/master/content",
-		"favicon": "https://graphql-engine-cdn.hasura.io/img/hasura_icon_black.svg"
-	},
+  gatsby: {
+    pathPrefix: "/",
+    siteUrl: "https://design.oslopride.no/",
+    gaTrackingId: null
+  },
+  header: {
+    logo:
+      "https://cdn.sanity.io/images/2ger3rla/prod2019/b3fba7f838b94613135cc39dec69cadae4b9bfd5-2000x2000.svg",
+    logoLink: "https://design.oslopride.no/",
+    title: "Design Guidelines",
+    githubUrl: "https://github.com/oslopride/design",
+    helpUrl: "",
+    tweetText: "",
+    links: [{ text: "", link: "" }],
+    search: {
+      enabled: false,
+      indexName: "",
+      algoliaAppId: process.env.GATSBY_ALGOLIA_APP_ID,
+      algoliaSearchKey: process.env.GATSBY_ALGOLIA_SEARCH_KEY,
+      algoliaAdminKey: process.env.ALGOLIA_ADMIN_KEY
+    }
+  },
+  sidebar: {
+    forcedNavOrder: ["/introduction", "/codeblock"],
+    links: [{ text: "Oslo Pride", link: "https://www.oslopride.no/" }],
+    frontline: false,
+    ignoreIndex: true
+  },
+  siteMetadata: {
+    title: "Design Guidelines | Oslo Pride",
+    description: "Documentation built with mdx",
+    ogImage: null,
+    docsLocation: "https://github.com/oslopride/design/blob/master/content",
+    favicon:
+      "https://cdn.sanity.io/images/2ger3rla/prod2019/b3fba7f838b94613135cc39dec69cadae4b9bfd5-2000x2000.svg"
+  }
 };
 
 module.exports = config;
